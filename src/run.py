fcd2338c3632bd9b10859071928e10b279591079
Apenz1 <noah.ed.peeters@gmail.com>
2021-10-28 21:06:53 +0200

    Implement qt_material dark theme

--- src/run.py
+++ src/run.py
@@ -1,5 +1,6 @@
 import sys
 import PySide6.QtWidgets as qtw
+from qt_material import apply_stylesheet
 
 from MainWindow.MainWindow import Window
 
@@ -7,5 +8,10 @@ from MainWindow.MainWindow import Window
 if __name__ == "__main__":
     app = qtw.QApplication(sys.argv)
     window = Window()
+
+    # TODO: Make setting toggle that saves stylesheet
+    # Setup stylesheet
+    apply_stylesheet(app, "dark_amber.xml")
+
     window.show()
     sys.exit(app.exec())
