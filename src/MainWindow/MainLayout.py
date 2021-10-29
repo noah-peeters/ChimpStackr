6a17ee5cf19046c87942a503e96a2779e9a87369
Apenz1 <noah.ed.peeters@gmail.com>
2021-10-29 20:46:32 +0200

    Added image viewer to MainWindow splitter

--- src/MainWindow/MainLayout.py
+++ src/MainWindow/MainLayout.py
@@ -3,10 +3,14 @@
 """
 import PySide6.QtWidgets as qtw
 
+from MainWindow.ImageViewer import ImageViewer
+
 
 class CenterWidget(qtw.QWidget):
     def __init__(self, main_window):
         super().__init__()
+
+        # Loaded images list widget
         loaded_images_list = qtw.QListWidget()
         # loaded_images_list.setAlternatingRowColors(True)
         loaded_images_list.setSelectionMode(qtw.QAbstractItemView.ExtendedSelection)
@@ -16,11 +20,15 @@ class CenterWidget(qtw.QWidget):
                 "Please load them in from the 'file' menu.",
             ]
         )
+        
+        # Image display widget
+        image_display = ImageViewer()
 
         # Create splitter
         splitter = qtw.QSplitter()
         splitter.setChildrenCollapsible(False)
         splitter.addWidget(loaded_images_list)
+        splitter.addWidget(image_display)
 
         # Set horizontal layout
         layout = qtw.QHBoxLayout(self)
