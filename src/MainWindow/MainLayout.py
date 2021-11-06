fe988af54479cab2ae7ca076b117f50341a79710
Apenz1 <noah.ed.peeters@gmail.com>
2021-11-06 20:07:56 +0100

    Loaded images widget with title implementation

--- src/MainWindow/MainLayout.py
+++ src/MainWindow/MainLayout.py
@@ -5,60 +5,57 @@
 import os
 import PySide6.QtWidgets as qtw
 import PySide6.QtCore as qtc
+import PySide6.QtGui as qtg
 
 from MainWindow.ImageViewer import ImageViewer
 from utilities import int_string_sorting
 
 
-class LoadedImagesWidget(qtw.QListWidget):
-    def __init__(self):
-        super().__init__()
-        self.default_loaded_images_items = [
-            "Loaded images will appear here.",
-            "Please load them in from the 'file' menu.",
-        ]
-        self.setSelectionMode(
-            qtw.QAbstractItemView.ExtendedSelection
-        )
-        self.setAlternatingRowColors(True)
-        
-        self.header_label = qtw.QLabel("Source images")
-
-        v_layout = qtw.QVBoxLayout()
-        v_layout.addWidget(self.header_label)
-        v_layout.addWidget(self)
-        self.setLayout(v_layout)
-
-
-class CenterWidget(qtw.QWidget):
+class LoadedImagesWidget(qtw.QWidget):
     default_loaded_images_items = [
         "Loaded images will appear here.",
         "Please load them in from the 'file' menu.",
     ]
+    default_header_text = "Source images"
 
+    def __init__(self):
+        super().__init__()
+        self.header_label = qtw.QLabel(self.default_header_text)
+
+        self.list = qtw.QListWidget()
+        self.list.setSelectionMode(qtw.QAbstractItemView.ExtendedSelection)
+        self.list.addItems(self.default_loaded_images_items)
+
+        v_layout = qtw.QVBoxLayout()
+        v_layout.addWidget(self.header_label)
+        v_layout.addWidget(self.list)
+        self.setLayout(v_layout)
+
+    # Reset list and header label to default values
+    def reset_to_default(self):
+        self.list.clear()
+        self.list.addItems(self.default_loaded_images_items)
+
+        self.header_label.setText(self.default_header_text)
+
+
+class CenterWidget(qtw.QWidget):
     def __init__(self, main_window):
         super().__init__()
-
-        # Loaded images list widget
-        self.loaded_images_list = qtw.QListWidget()
-        # loaded_images_list.setAlternatingRowColors(True)
-        self.loaded_images_list.setSelectionMode(
-            qtw.QAbstractItemView.ExtendedSelection
-        )
-        self.loaded_images_list.addItems(self.default_loaded_images_items)
+        self.loaded_images_widget = LoadedImagesWidget()
 
         # Image display widget
-        image_display = ImageViewer(self.loaded_images_list)
+        image_display = ImageViewer(self.loaded_images_widget.list)
 
         # Create splitter
         splitter = qtw.QSplitter()
         splitter.setChildrenCollapsible(False)
-        splitter.addWidget(self.loaded_images_list)
+        splitter.addWidget(self.loaded_images_widget)
         splitter.addWidget(image_display)
 
         # Set splitter default size
         width = main_window.screen().availableGeometry().width()
-        splitter.setSizes([int(width / 5), width])
+        splitter.setSizes([int(width / 3.5), width])
 
         # Set horizontal layout
         layout = qtw.QHBoxLayout(self)
@@ -68,16 +65,21 @@ class CenterWidget(qtw.QWidget):
     # Called from parent MainWindow. Process image path names and display new loaded images inside list widget.
     # QListWidget data is set to the full image path for quick retrieval later.
     def set_loaded_images(self, new_image_files):
-        self.loaded_images_list.clear()
+        self.loaded_images_widget.reset_to_default()
         if len(new_image_files) <= 0:
-            # No images selected, set default
-            self.loaded_images_list.addItems(self.default_loaded_images_items)
+            # No images selected, use default
             return
 
-        # Set new files (if there)
+        # New images selected: update list and header text
+        self.loaded_images_widget.header_label.setText(
+            "Source images (" + str(len(new_image_files)) + ")"
+        )
+
+        # Set new files
+        self.loaded_images_widget.list.clear()
         for path in sorted(new_image_files, key=int_string_sorting):
             name = os.path.basename(path)
             item = qtw.QListWidgetItem()
             item.setData(qtc.Qt.UserRole, path)  # Set data to full image path
             item.setText(name)  # Set text to image name
-            self.loaded_images_list.addItem(item)
+            self.loaded_images_widget.list.addItem(item)
