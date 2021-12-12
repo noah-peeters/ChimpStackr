faa0ab63508118837e803e81fe2763eb846ebf9b
Apenz1 <noah.ed.peeters@gmail.com>
2021-12-12 10:38:15 +0100

    Small changes

---
+++ src/algorithm/ProgressLoggingHandler.py
@@ -0,0 +1,22 @@
+"""
+Class that connects to QtSignals. Keeps track of timing of different parts of stacking.
+Data can be retrieved once finished, to display in a graph.
+"""
+
+class Logger:
+    def __init__(self, signals):
+        self.signals = signals
+        signals.finished.connect(self.finished)
+        signals.progress_update.connect(self.progress_update)
+        signals.status_update.connect(self.status_update)
+    
+    # Handle final data
+    def finished(self):
+        print()
+
+    # Handle previous process data
+    def progress_update(self, current_progress_percentage):
+        print()
+    
+    def status_update(self, status_text):
+        print()
