faf067d781ee82f2fcecd41bd19902465cac48f8
Apenz1 <noah.ed.peeters@gmail.com>
2021-12-12 10:19:34 +0100

    Implemented button box inside stack statistics

--- src/MainWindow/StackSuccessDialog.py
+++ src/MainWindow/StackSuccessDialog.py
@@ -12,9 +12,15 @@ class StatsWindow(qtw.QDialog):
         time_taken_label = qtw.QLabel("Operation took: HH:MM:SS")
         stat2 = qtw.QLabel("Stat 2 test label")
 
+        # Button box
+        button_box = qtw.QDialogButtonBox()
+        button_box.clicked.connect(lambda: self.close())
+        button_box.addButton(qtw.QDialogButtonBox.Ok)
+
         v_layout = qtw.QVBoxLayout()
         v_layout.addWidget(time_taken_label)
         v_layout.addWidget(stat2)
+        v_layout.addWidget(button_box)
 
         self.setLayout(v_layout)
         self.setWindowTitle("Stacking stats")
