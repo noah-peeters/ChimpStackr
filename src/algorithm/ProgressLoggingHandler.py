a89610bc39b8261209701c37cad1383e12ec31fd
Apenz1 <noah.ed.peeters@gmail.com>
2021-12-15 19:13:54 +0100

    Small changes

--- src/algorithm/ProgressLoggingHandler.py
+++ src/algorithm/ProgressLoggingHandler.py
@@ -3,20 +3,23 @@ Class that connects to QtSignals. Keeps track of timing of different parts of st
 Data can be retrieved once finished, to display in a graph.
 """
 
+# TODO: Implement time log
 class Logger:
+    time_stamps = {}
+
     def __init__(self, signals):
         self.signals = signals
         signals.finished.connect(self.finished)
         signals.progress_update.connect(self.progress_update)
-        signals.status_update.connect(self.status_update)
-    
+        # signals.status_update.connect(self.status_update)
+
     # Handle final data
     def finished(self):
         print()
 
     # Handle previous process data
     def progress_update(self, current_progress_percentage):
-        print()
-    
-    def status_update(self, status_text):
-        print()
+        print(current_progress_percentage)
+
+    # def status_update(self, status_text):
+    #     print()
