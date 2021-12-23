601b044505dbe675e6f7ce1c30157906e27ce711
Apenz1 <noah.ed.peeters@gmail.com>
2021-12-23 13:10:06 +0100

    Add low res images for testing stack

--- tests/test_algorithm_API.py
+++ tests/test_algorithm_API.py
@@ -3,13 +3,25 @@ Test multiple functions of algorithm API.
 """
 # Hack to import modules from src
 # src: https://codeolives.com/2020/01/10/python-reference-module-in-parent-directory/
-import os, sys
+import os, sys, tempfile
 
 currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
 
-from src.algorithm import image_storage
+from src.algorithm import API
+
+ROOT_TEMP_DIRECTORY = tempfile.TemporaryDirectory(prefix="FocusStacking_")
+
+laplacian_pyramid_algorithm = API.LaplacianPyramid(ROOT_TEMP_DIRECTORY)
+
+# Test image loading (+clearing)
+
+# TODO: Test aligning images
+
+# Test stacking images
+
+# Test output export write to disk
 
 
-# Test saving and loading pyramid  to/from disk
+ROOT_TEMP_DIRECTORY.cleanup()
