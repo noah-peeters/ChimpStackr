cb5b5a2d1bef471b7f257b5818736efcbb007021
noah-peeters <noah.ed.peeters@gmail.com>
2022-02-09 20:39:43 +0100

    Add application icon + improved icon

--- src/run.py
+++ src/run.py
@@ -1,5 +1,6 @@
 import os, sys, tempfile
 import PySide6.QtWidgets as qtw
+import PySide6.QtGui as qtg
 
 # Hack to allow imports from src/ example: "src.algorithm.API"
 # src: https://codeolives.com/2020/01/10/python-reference-module-in-parent-directory/
@@ -15,11 +16,19 @@ ROOT_TEMP_DIRECTORY = tempfile.TemporaryDirectory(prefix="FocusStacking_")
 settings.init()
 settings.globalVars["RootTempDir"] = ROOT_TEMP_DIRECTORY
 
+
+# Taskbar icon fix for Windows 7
+# Src: https://stackoverflow.com/questions/1551605/how-to-set-applications-taskbar-icon-in-windows-7S
+import ctypes
+myappid = 'mycompany.myproduct.subproduct.version' # arbitrary string
+ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
+
 if __name__ == "__main__":
     app = qtw.QApplication(sys.argv)
     # Needed for saving QSettings
     app.setApplicationName("ChimpStackr")
     app.setOrganizationName("noah.peeters")
+    app.setWindowIcon(qtg.QIcon("snap/gui/icon.png"))
 
     settings.globalVars["MainApplication"] = app
 
