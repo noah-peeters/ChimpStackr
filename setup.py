9238deb15e18b148774ba7ad4d23504e4b2c6efa
Noah Peeters <noah.ed.peeters@gmail.com>
2022-03-04 20:46:11 +0100

    test snapcraft-preload

--- setup.py
+++ setup.py
@@ -4,23 +4,23 @@ import src
 #     long_description = fh.read()
 
 setuptools.setup(
-    name="chimpstackr",
+    # name="chimpstackr",
     version=src.__version__,
-    author="Noah Peeters",
-    author_email="apenz1.peeters@gmail.com",
+    # author="Noah Peeters",
+    # author_email="apenz1.peeters@gmail.com",
     # description="Focus Stacking software",
     # long_description=long_description,
     # long_description_content_type="text/markdown",
-    url="https://github.com/noah-peeters/ChimpStackr",
-    project_urls={
-        "Bug Tracker": "https://github.com/noah-peeters/ChimpStackr/issues",
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: OS Independent",
-    ],
+    # url="https://github.com/noah-peeters/ChimpStackr",
+    # project_urls={
+    #     "Bug Tracker": "https://github.com/noah-peeters/ChimpStackr/issues",
+    # },
+    # classifiers=[
+    #     "Programming Language :: Python :: 3",
+    #     "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    #     "Operating System :: OS Independent",
+    # ],
+    # python_requires=">=3.6",
     packages=setuptools.find_packages(),
-    python_requires=">=3.6",
-    entry_points={"console_scripts": ["app=src.run:main"]},
+    entry_points={"console_scripts": ["chimpstackr=src.run:main"]},
 )
