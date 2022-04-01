5ad04851f3a70544fdeda71a4c4c973a1388b2cc
noah-peeters <noah.ed.peeters@gmail.com>
2022-04-01 17:25:53 +0200

    Pyramid focusmap fusion: don't create new array (expensive)

--- src/algorithm/algorithm.py
+++ src/algorithm/algorithm.py
@@ -104,14 +104,15 @@ def compute_focusmap(pyr_level1, pyr_level2, kernel_size):
     cache=True,
 )
 def fuse_pyramid_levels_using_focusmap(pyr_level1, pyr_level2, focusmap):
-    output = np.empty_like(pyr_level1)
+    # Copy directly in "pyr_level_1",
+    # as creating a new array using ".copy()" takes longer
     for y in nb.prange(focusmap.shape[0]):
         for x in nb.prange(focusmap.shape[1]):
             if focusmap[y, x] == 0:
-                output[y, x, :] = pyr_level1[y, x, :]
+                pyr_level1[y, x, :] = pyr_level1[y, x, :]
             else:
-                output[y, x, :] = pyr_level2[y, x, :]
-    return output
+                pyr_level1[y, x, :] = pyr_level2[y, x, :]
+    return pyr_level1
 
 
 class Algorithm:
