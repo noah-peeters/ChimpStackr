364e1351db33661ad87ac0fc03ca6f3e09b6c472
Noah Peeters <noah.ed.peeters@gmail.com>
2022-10-10 10:33:59 +0200

    Time remaining keep a limited number in cache (automatically keeps that number of items)

--- src/MainWindow/TimeRemainingHandler.py
+++ src/MainWindow/TimeRemainingHandler.py
@@ -6,6 +6,8 @@ import time, statistics
 
 
 class TimeRemainingHandler:
+    num_to_keep_cached = 5  # Number of items to keep in cache
+
     def __init__(self):
         self.cached_time_taken = []  # List of cached time taken
 
@@ -13,15 +15,16 @@ class TimeRemainingHandler:
     def calculate_time_remaining(
         self, percentage_increment, percentage_left, time_taken
     ):
+
         self.cached_time_taken.append(time_taken)
         mean_time_taken = statistics.mean(self.cached_time_taken)
+
+        # Remove last item
+        if len(self.cached_time_taken) >= self.num_to_keep_cached:
+            self.cached_time_taken.pop(len(self.cached_time_taken) - 1)
 
         # Time left to 100% completion of current operation
         time_left = percentage_left / percentage_increment * mean_time_taken
 
         formatted = time.strftime("%H:%M:%S", time.gmtime(time_left * 1.75))
         return "Time left: " + formatted
-
-    # Remove cached vars
-    def clear_cache(self):
-        self.cached_time_taken.clear()
